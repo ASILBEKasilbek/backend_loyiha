6ae99a3a6982118d4105aea291d891e19adb8915
unknown <dildormingturaeva@gmail.com>
2024-12-11 20:34:19 +0500

    o'zgarish modellarda

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,44 +1,88 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    pass 
 class Seller(models.Model):
+
+
+    class Status(models.TextChoices):
+        NEW = 'new', 'yangi',
+        MODERATION = 'Modeartion', 'Moderatsiya'
+        COD_VERIFIED =  "cod_verified", 'kod tasdiqlangan'
+
     full_name=models.CharField(max_length=250)
     father_name=models.CharField(max_length=250)
     birth_date=models.DateField()
     register_certificate=models.FileField()
-    phone=models.IntegerField()
+    phone=phone = models.CharField(max_length=15, blank=True, null=True)
     expired_date=models.DateTimeField()
-    code=models.CharField(max_length=250)
-    # status=models.Choices()
-    latitude=models.FloatField()
-    longitude=models.FloatField()
-    # user=models.ForeignKey()
-class UserLocation(models.Model):
-    latitude=models.FloatField()
-    longitude=models.FloatField()
-    # user=models.ForeignKey()
-class Order(models.Model):
-    price=models.IntegerField()
-    # delivery_type=models.Choices()
-    # status=models.Choices()
+    code=models.CharField (max_length=250)
+    status= models.CharField (max_length=50, 
+    choices=Status.choices, 
+    default=Status.NEW
+    )
 
-class OrderItems(models.Model):
-    # product=models.ForeignKey()
-    # order=models.ForeignKey()
-    count=models.PositiveIntegerField()
-class ProductImage(models.Model):
-    image1=models.ImageField()
+    def __str__(self):
+        return self.full_name
+    
+class UserLocation(models.Model):
+        latitude=models.FloatField()
+        longitude=models.FloatField()
+        user=models.ForeignKey(User, on_delete=models.PROTECT)
+
+class Order(models.Model):
+    class Delivery_type(models.TextChoices):
+        MARKET = 'market', "do'kondan"
+        HOME =  "home", 'uydan'
+
+    class Status(models.TextChoices):
+        IN_PROCESS = 'proccess', "djarayonda "
+        PAID  =  "paid", "to'langan"
+
+    delivery_type =  models.CharField(max_length=250, choices=Status)
+    price=models.IntegerField()
+    status=models.Choices(models.CharField(max_length=250, choices=Status))
+
+    def __str__(self):
+        return self.price
+
+
 class Category(models.Model):
     name=models.CharField(max_length=250)
+
+class ProductImage(models.Model):
+        image1=models.ImageField()
+
+
 class Product(models.Model):
+     
+    class Status(models.TextChoices):
+        IN_PROCESS = 'proccess', "jarayonda "
+        PAID  =  "paid", "to'langan"
+        UNPAID = "unpaid" , "to'lanmagan"
+        CANCELLED = "cancelled", "bekor qilingan"
+  
+
     name=models.CharField(max_length=250)
     author=models.CharField(max_length=250)
-    # price=models.DecimalField()
+    price=models.DecimalField()
     count=models.PositiveIntegerField()
     description=models.CharField(max_length=250)
-    # status=models.Choices(names='4')
+    status=models.Choices(status=models.Choices(models.CharField(max_length=250, choices=Status)))
     comment=models.TextField()
-    # category=models.ForeignKey()
-    # images=models.ManyToManyField()
-class User(models.Model):
-    pass 
+    category=models.ForeignKey(Category, on_delete=models.PROTECT)
+    images=models.ManyToManyField()
+
+
+
+class OrderItems(models.Model):
+        product=models.ForeignKey(Product, on_delete=models.PROTECT)
+        order=models.ForeignKey(Order, on_delete=models.PROTECT )
+        count=models.PositiveIntegerField()
+
+        def __str__(self):
+            return self.count
+                                     
+
--- apps/common/urls.py
+++ apps/common/urls.py
@@ -3,13 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router=DefaultRouter()
-router.register(r'Sellers',SellerViewSet)
-router.register(r'Users',UserViewSet)
-router.register(r'UserItemss',UserLocationViewSet)
-router.register(r'Products',ProductViewSet)
-router.register(r'ProductImages',ProductImageViewSet)
-router.register(r'Orders',OrderViewSet)
-router.register(r'OrderItemss',OrderItemsViewSet)
+router.register(r'Sellers',SellerViewSet),
+router.register(r'Users',UserViewSet),
+router.register(r'UserItemss',UserLocationViewSet),
+router.register(r'Products',ProductViewSet),
+router.register(r'ProductImages',ProductImageViewSet),
+router.register(r'Orders',OrderViewSet),
+router.register(r'OrderItemss',OrderItemsViewSet),
 router.register(r'Categorys',CategoryViewSet)
 
 
